a47557c25cd500ff11b9dfc77ab4b417f056d780
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-12-19 18:25:51 +0100

    move certbot command to entrypoint script

--- certbot/docker-entrypoint.sh
+++ certbot/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# Run certbot with all arguments passed to the script
-certbot "$@"
+certbot certonly \
+    --webroot \
+    --webroot-path=/var/www/certbot \
+    --email "${CERTBOT_EMAIL}" \
+    -d thrivingindividuals.org \
+    -d www.thrivingindividuals.org \
+    --agree-tos \
+    --non-interactive
 
 # Fix permissions after certbot runs
 chown -R 1000:1000 /etc/letsencrypt
