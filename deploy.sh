b5ca2c3a7babd1ff7c84620ad2c652e9db8e2ef4
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-12-22 22:03:08 +0100

    use two-tiered deployment for certbot

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Initial deployment with HTTP-only config
+cp nginx/conf.d/http.conf nginx/conf.d/default.conf
+docker-compose up -d nginx sveltekit pocketbase
+
+# Wait for nginx to be ready
+sleep 5
+
+# Get SSL certificates
+docker-compose up certbot
+
+# Switch to HTTPS config if certificates were obtained
+if [ -f "certbot/conf/live/thrivingindividuals.org/fullchain.pem" ]; then
+    cp nginx/conf.d/https.conf nginx/conf.d/default.conf
+    docker-compose restart nginx
+fi
