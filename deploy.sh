c176e21b0e02beb7216b377a657780a994ee6d1a
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-12-24 13:25:45 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ cp nginx/conf.d/http.conf nginx/conf.d/default.conf
 docker-compose up -d nginx sveltekit pocketbase
 
 # Wait for nginx to be ready
-sleep 5
+sleep 10
 
 # Get SSL certificates
-docker-compose up certbot
+docker-compose run --rm certbot
 
 # Switch to HTTPS config if certificates were obtained
 if [ -f "certbot/conf/live/thrivingindividuals.org/fullchain.pem" ]; then
