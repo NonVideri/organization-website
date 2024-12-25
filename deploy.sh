aa5c688d44a0e7b205ed014432aaaba6067bd733
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2024-12-25 17:15:52 +0100

    move configs to nginx folder by default

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Initial deployment with HTTP-only config
-cp nginx/conf.d/http.conf nginx/conf.d/default.conf
+cp nginx/http.conf nginx/conf.d/default.conf
 docker-compose up -d nginx sveltekit pocketbase
 
 # Wait for nginx to be ready
-sleep 20
+sleep 10
 
 # Get SSL certificates
 docker-compose run --rm certbot
 
 # Switch to HTTPS config if certificates were obtained
 if [ -f "certbot/conf/live/thrivingindividuals.org/fullchain.pem" ]; then
-    cp nginx/conf.d/https.conf nginx/conf.d/default.conf
+    cp nginx/https.conf nginx/conf.d/default.conf
     docker-compose restart nginx
 fi
