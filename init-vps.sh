9e01d6e3b35bee832729d2d5e89d510c5317fb23
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2025-04-24 18:23:12 +0200

    add more to init-vps script

--- init-vps.sh
+++ init-vps.sh
@@ -1,6 +1,22 @@
-#!/bin/bash
-# init-vps.sh
+# Not to be executed, just a list of commands to recreate VPS
 
+# Add admin user
+useradd administrator
+passwd administrator
+echo "administrator ALL=(ALL) ALL" >> /etc/sudoers
+
+# Copy SSH keys from local
+mkdir -p /home/administrator/.ssh
+vim /home/administrator/.ssh/authorized_keys
+
+# Disable password and root login
+sudo vim /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+# Relog as admin
+su administrator
+
+# Install dependencies
 sudo apt-get update
 sudo apt-get install -y rsync docker.io docker-compose
 sudo usermod -aG docker $USER
