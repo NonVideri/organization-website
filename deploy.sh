7d346d24758e5e03b361bab50c7fb4719c51a767
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2025-05-04 23:30:52 +0200

    modify PDF serving

--- deploy.sh
+++ deploy.sh
@@ -154,15 +154,8 @@ server {
 
     # PDF serving
     location ~* \.pdf$ {
-        proxy_pass http://sveltekit:3000;
-        proxy_http_version 1.1;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        
-        # Override X-Frame-Options to allow PDFs in iframes
-        proxy_hide_header X-Frame-Options;
+        root /opt/app/sveltekit/static;
+        try_files $uri =404;
         add_header X-Frame-Options "SAMEORIGIN" always;
         add_header Content-Security-Policy "frame-ancestors 'self';" always;
     }
