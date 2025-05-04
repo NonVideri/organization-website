6b94ca7699d23fdb5c53fbc43bbb334930285917
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2025-05-04 22:18:23 +0200

    update SubscribeForm look

--- sveltekit/src/lib/Header.svelte
+++ sveltekit/src/lib/Header.svelte
@@ -22,7 +22,7 @@
 {#if type === 'h1'}
 	<h1
 		class={cn(
-			'text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl mb-2 md:mb-5',
+			'text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl mb-3 md:mb-5',
 			HEADER_CLASSES,
 			className
 		)}>
@@ -31,7 +31,7 @@
 {:else if type === 'h2'}
 	<h2
 		class={cn(
-			'text-xl md:text-2xl xl:text-3xl 2xl:text-4xl mb-2 md:mb-5',
+			'text-xl md:text-2xl xl:text-3xl 2xl:text-4xl mb-3 md:mb-5',
 			HEADER_CLASSES,
 			className
 		)}>
@@ -40,7 +40,7 @@
 {:else if type === 'h3'}
 	<h3
 		class={cn(
-			'text-lg md:text-xl xl:text-2xl 2xl:text-3xl mb-1 md:mb-3',
+			'text-lg md:text-xl xl:text-2xl 2xl:text-3xl mb-3',
 			HEADER_CLASSES,
 			className
 		)}>
@@ -49,7 +49,7 @@
 {:else if type === 'h4'}
 	<h4
 		class={cn(
-			'text-base md:text-lg xl:text-xl 2xl:text-2xl mb-1 md:mb-2',
+			'text-base md:text-lg xl:text-xl 2xl:text-2xl mb-3',
 			HEADER_CLASSES,
 			className
 		)}>
@@ -58,7 +58,7 @@
 {:else if type === 'h5'}
 	<h5
 		class={cn(
-			'text-sm md:text-base xl:text-lg 2xl:text-xl mb-0.5 md:mb-1',
+			'text-sm md:text-base xl:text-lg 2xl:text-xl mb-1',
 			HEADER_CLASSES,
 			className
 		)}>
