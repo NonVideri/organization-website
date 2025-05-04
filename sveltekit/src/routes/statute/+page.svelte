ce53a30df8ca65eaefe628ccd42abbba2df7585f
Christopher Mael <krzysztof.adam.moszynski@gmail.com>
2025-05-04 21:53:59 +0200

    add title to statute page

--- sveltekit/src/routes/statute/+page.svelte
+++ sveltekit/src/routes/statute/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	import SvelteSeo from 'svelte-seo';
+
+  const TITLE = 'Thriving Individuals Foundation Statute';
+</script>
+
+<SvelteSeo title={TITLE} />
+
 <iframe
 	src="/statute.pdf"
 	class="w-full h-screen"
